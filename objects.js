285ca6210abcfaee3b65d6c82bb508f570ac244d
MLUCAN2 <162901593+MLUCAN2@users.noreply.github.com>
2024-07-29 11:32:11 -0500

    added objects folder

---
+++ objects.js
@@ -0,0 +1,17 @@
+
+
+const ourDog= {
+    "Name": "Turner",
+    "Legs": 4,
+    'Tails': 1,
+    'Breed': 'German Sheppard'
+};
+
+const myDog= {
+    'Name': 'Charles',
+    'Legs': 2,
+    'Tails': 2,
+    'Breed': 'Bull Terrier'
+};
+
+console.log(myDog, ourDog)
--- script.js
+++ script.js
@@ -390,6 +390,40 @@ function sequentialSize(val){
 
 console.log(sequentialSize(9));
 
+// Creating a card counting function for blackjack
+let count=0;
+
+function cc(card){
+    switch(card){
+        // Low cards
+        case 2:
+        case 3:
+        case 4:
+        case 5:
+        case 6:
+            count++;
+            break;
+        case 10:
+        case 'J':
+        case 'Q':
+        case 'K':
+        case 'A':
+            count--
+            break;
+    }
+    let holdbet= 'Hold'
+    if (count > 0){
+        holdbet='Bet'
+    }
+    return count + ' ' + holdbet
+}
+cc(2);
+cc('K');
+cc(10);
+cc('K');
+cc('A')
+
+console.log(cc(4))
 
 
 
