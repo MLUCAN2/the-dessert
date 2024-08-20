5022cc84e1513c504af5fb105b022ea7482be355
MLUCAN2 <162901593+MLUCAN2@users.noreply.github.com>
2024-08-20 10:11:13 -0500

    added to the functions.js and added new files for DOM instruction

--- functions.js
+++ functions.js
@@ -1,3 +1,4 @@
+// For more functions go to https://www.w3schools.com/js/js_functions.asp
 // Functions are the building blocks of an app that allow us to group together statements to perform a task or calculate a value
 // Function is the keyword, then the variable with (passed in or empty argument or parameter variable) then { our defined value(s) }
 // function sayHi(){
@@ -61,5 +62,83 @@
 // console.log(txtMessage('Seymours your maaaan'))
 
 
+// Generating a random number
+// Step 1
+// function randomNum(min, max){
+//     var numOfValues= (max-min+1);
+//     return numOfValues;
+// }
+// console.log(randomNum(86, 1000))
 
-console.log(Math.random())
+// Step 2: incorporating Math.random()
+// function randomNum(min, max){
+//     var sumOf= (max-min+1);
+//     var randomGroup= Math.random();
+//     var equation= sumOf*randomGroup
+//     return equation
+// }
+// console.log(randomNum(2, 5))
+
+// Step 3: round the numbers using Math.floor
+// function randomNum(min, max){
+//     var sumOf= (max-min+1);
+//     var randomGroup=Math.random();
+//     var equation= (sumOf*randomGroup);
+//     var rounded= Math.floor(equation);
+//     return rounded
+// }
+// console.log(randomNum(3,500))
+
+// Step 4: add min to the rounded value that we generated
+// function randomNum(min, max){
+//     var sumOf= (max-min+1);
+//     var randomGroup= Math.random();
+//     var equation=(randomGroup*sumOf);
+//     var rounded= Math.floor(equation);
+//     var final= (rounded+min)
+//     return final
+// }
+// We have established a range, so the number will not go past 5 or 10
+// console.log(randomNum(5, 10))
+
+// Step 5: incorporate an array so that the random number generator picks something out
+// var colors=['red', 'orange', 'yellow', 'green', 'blue', 'purple', 'violet', 'brown', 'black']
+
+// function randomColor(min, max){
+//     var sumOf= (max-min+1);
+//     var random= Math.random();
+//     var equation= random*sumOf;
+//     var rounded= Math.floor(equation)
+//     var final= (rounded+min)
+//     return final
+// }
+// console.log(colors[randomColor(0,9)])
+
+// Step 6: Refactor the code
+var colors=['red', 'orange', 'yellow', 'green', 'blue', 'purple', 'violet', 'brown', 'black']
+
+function randomColor(min, max){
+    return Math.floor((max-min+1) * Math.random())+min
+}
+console.log(colors[randomColor(0,9)])
+
+
+// Making an anonymous function and assigning it to a variable
+// var greeting= function(){
+//     return ('Hello from the anonymous function')
+// }
+// console.log(greeting());     
+
+// To calculate absolute difference
+//In the function below, we are finding the absolute value by subtracting n from 21.
+// If the number is higher than 21 than we get the difference and then double it.
+function absDiff(n){
+    if (n<=21){
+        return Math.abs(21-n)
+    }
+    else{
+        return Math.abs(n-21)*2
+    }
+}
+console.log(absDiff(2))
+
--- script.js
+++ script.js
@@ -1,101 +1,131 @@
-// Challenge 1: Write your name so that it will appear in the console.
-let firstName= 'Myron';
-let lastName= 'Lucan';
+// // Challenge 1: Write your name so that it will appear in the console.
+// let firstName= 'Myron';
+// let lastName= 'Lucan';
 
-console.log(`${firstName} ${lastName}`)
+// console.log(`${firstName} ${lastName}`)
 
-// Challenge 2: Write out a string with an adjective about yourself. Concatenate it with your name
-let aboutMe= `Hi! My name is ${firstName} ${lastName} and I am inconsistently funny!`;
-console.log(aboutMe);
+// // Challenge 2: Write out a string with an adjective about yourself. Concatenate it with your name
+// let aboutMe= `Hi! My name is ${firstName} ${lastName} and I am inconsistently funny!`;
+// console.log(aboutMe);
 
-/*Challenge 3: Write script that prompts the user for their name, and
-then display a sentence in the console that greets the
-user by name and welcomes them. */
-// var name= prompt('Whats your name friend?')
-// if(name !== ''){
-//     console.log(`Good to meet you ${name}`)
-// }
-// else{
-//     console.log('Please refresh and enter your name.')
-// }
+// /*Challenge 3: Write script that prompts the user for their name, and
+// then display a sentence in the console that greets the
+// user by name and welcomes them. */
+// // var name= prompt('Whats your name friend?')
+// // if(name !== ''){
+// //     console.log(`Good to meet you ${name}`)
+// // }
+// // else{
+// //     console.log('Please refresh and enter your name.')
+// // }
 
-/* Challenge 4: Use prompt and ask the user for a
-whole number. If they type in a number less
-than 10, have the console log report, "You
-entered a number smaller than 10",
-otherwise, send "You entered a big number"
-to the console. */
+// /* Challenge 4: Use prompt and ask the user for a
+// whole number. If they type in a number less
+// than 10, have the console log report, "You
+// entered a number smaller than 10",
+// otherwise, send "You entered a big number"
+// to the console. */
 
-// var number= prompt('Please enter a whole number', );
-// if( number < 10){
-//     console.log('You entered a number lower than 10')
-// }
-// else if (number > 9){
-//     console.log('You entered a number greater than 10')
-// }
-// else if (number == 10){
-//     console.log(`Congrats, you entered ${number}, you're a winner!`)
-// }
-// else if (number){
-//     console.log('Well you have to enter something silly')
-// }
-// else {
-//     console.error('Thats not a number you goose');
+// // var number= prompt('Please enter a whole number', );
+// // if( number < 10){
+// //     console.log('You entered a number lower than 10')
+// // }
+// // else if (number > 9){
+// //     console.log('You entered a number greater than 10')
+// // }
+// // else if (number == 10){
+// //     console.log(`Congrats, you entered ${number}, you're a winner!`)
+// // }
+// // else if (number){
+// //     console.log('Well you have to enter something silly')
+// // }
+// // else {
+// //     console.error('Thats not a number you goose');
     
-// }
+// // }
 
-/* Challenge 5: If the user types a number
-less than 10, use a loop to print out the
-numbers from zero to 9 in the console. */ 
+// /* Challenge 5: If the user types a number
+// less than 10, use a loop to print out the
+// numbers from zero to 9 in the console. */ 
 
 
-// var number= prompt('Please enter a whole number', );
-// if( number < 10){
-//     console.log('You entered a number less than 10')
-//     for (var i=0; i < 10; i++){
-//         if (i == number){
-//             console.log(`Here is a ${i}  <---This is your number`)
-//         }
-//         else{
-//             console.log(`Here is a ${i}`)
-//         }
+// // var number= prompt('Please enter a whole number', );
+// // if( number < 10){
+// //     console.log('You entered a number less than 10')
+// //     for (var i=0; i < 10; i++){
+// //         if (i == number){
+// //             console.log(`Here is a ${i}  <---This is your number`)
+// //         }
+// //         else{
+// //             console.log(`Here is a ${i}`)
+// //         }
+// //     }
+// // }
+// // else if (number > 10){
+// //     console.log('You entered a number greater than 10')
+// // }
+// // else if (number == 10){
+// //     console.log(`Congrats, you entered ${number}, you're a winner!`)
+// // }
+// // else if (''){
+// //     console.log('Well you have to enter something silly')
+// // }
+// // else {
+// //     console.error('Thats not a number you goose');
+    
+// // }
+
+// // We can also imbed the loop into a function
+// // function numberFinder(){
+// //     var number= prompt(`Please enter a number:`);
+// //     if(number < 10 && number > 0){
+// //         for( var i=0; i<10; i++){
+// //             if (i == number){
+// //                 console.log(`Here is a ${i} <---This is what you chose`);
+// //             }
+// //             else{
+// //                 console.log(`Here is a ${i}`);
+// //             }
+// //         }
+// //     }
+// //     else if(number > 10){
+// //         console.log(`You chose ${number}, that was bigger than expected`)
+// //     }
+// //     else if( number == 10){
+// //         console.log(`Congrats, you are a winner!`)
+// //     }
+// //     else {
+// //         console.log('You need to enter a number');
+// //     }
+// // }
+// // numberFinder();
+
+
+// function hello(name){
+//     var name= prompt('Please enter your name')
+//     if (name == '' || name == null){
+//         console.log('You need to enter your name')
+//     }else{
+//          var greeting= `Hello ${name}, and welcome!`
+//         return name
 //     }
-// }
-// else if (number > 10){
-//     console.log('You entered a number greater than 10')
-// }
-// else if (number == 10){
-//     console.log(`Congrats, you entered ${number}, you're a winner!`)
-// }
-// else if (''){
-//     console.log('Well you have to enter something silly')
-// }
-// else {
-//     console.error('Thats not a number you goose');
     
-// }
+// };
 
-// We can also imbed the loop into a function
-function numberFinder(){
-    var number= prompt(`Please enter a number:`);
-    if(number < 10 && number > 0){
-        for( var i=0; i<10; i++){
-            if (i == number){
-                console.log(`Here is a ${i} <---This is what you chose`);
-            }
-            else{
-                console.log(`Here is a ${i}`);
-            }
-        }
-    }
-    else if(number > 10){
-        console.log(`You chose ${number}, that was bigger than expected`)
-    }
-    else if( number == 10){
-        console.log(`Congrats, you are a winner!`)
-    }
-    else {
-        console.log('You need to enter a number');
-    }
-}
-numberFinder();
+// function randomSelection(min, max){
+//     return Math.floor((max-min+1) * Math.random())+min
+// };
+
+// var foods=['cheese', 'pears', 'apples', 'almonds', 'cake', 'brownies'];
+// function hospitality(){
+//     var name= hello()
+//     if (name){
+//         console.log(`Hello ${name}, and welcome!`)
+//         var randomFood= foods[randomSelection(0, foods.length-1)]
+//         console.log(`${name}, would you like some ${randomFood}?`)
+//     }
+// };
+
+// hospitality()
+
+
