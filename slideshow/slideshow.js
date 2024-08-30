af24f64d48518c474ad6f9dfa902ea82d98da8a8
MLUCAN2 <162901593+MLUCAN2@users.noreply.github.com>
2024-08-30 09:18:31 -0500

    updated slideshow

--- slideshow/slideshow.js
+++ slideshow/slideshow.js
@@ -3,25 +3,30 @@
 function imageSwitcher(){
     'useStrict'
     const myImages= ['images/image1.jpg', 'images/image2.jpg', 'images/image3.jpg', 'images/image4.jpg', 'images/image5.jpg'];
+    const imageCont= document.getElementById('images');
     let currentImg= 0;
 
-    document.getElementById('next').onclick= nextPic;
-    document.getElementById('previous').onclick= prePic;
+    const nextSlide= document.getElementById('next');
+    const preSlide= document.getElementById('previous');
 
-    function nextPic(){
+    function nextPic(e){
         currentImg++;
         if (currentImg > myImages.length-1){
             currentImg= 0
         }
         document.getElementById('myImage').src=myImages[currentImg];
+        e.preventDefault();
     }
+    nextSlide.addEventListener('click', nextPic)
 
-    function prePic(){
+    function prePic(e){
         currentImg--;
         if (currentImg < 0){
             currentImg= myImages.length-1
         }
         document.getElementById('myImage').src=myImages[currentImg];
+        e.preventDefault();
     }
+    preSlide.addEventListener('click',prePic)
 };
 imageSwitcher();
